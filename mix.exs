4377df3fcfb6de536705857f2b0c5f17c7e917f1
Mateusz Front <mateusz.front@swmansion.com>
2019-08-08 12:56:03 +0200

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.IBMSpeechToText.MixProject do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.1.3"
   @github_url "https://github.com/membraneframework/membrane-element-ibm-speech-to-text"
 
   def project do
