4db7d469ae994ce374d9fd4970ed496fa323d481
Radosław Szuma <radoslawszuma@gmail.com>
2022-05-13 15:33:13 +0200

    Update core to 0.10.0

--- mix.exs
+++ mix.exs
@@ -37,11 +37,11 @@ defmodule Membrane.Element.IBMSpeechToText.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.8.0"},
+      {:membrane_core, "~> 0.10.0"},
       {:membrane_caps_audio_flac, "~> 0.1.1"},
       {:ibm_speech_to_text, "~> 0.3.0"},
-      {:membrane_file_plugin, "~> 0.7.0", only: [:dev, :test]},
-      {:membrane_flac_plugin, "~> 0.7.0", only: [:dev, :test]},
+      {:membrane_file_plugin, "~> 0.12.0", only: [:dev, :test]},
+      {:membrane_flac_plugin, "~> 0.8.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
