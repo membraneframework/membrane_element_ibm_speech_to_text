f150cf7e9b145304bbd6552b8285584a82b5e049
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:03 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -43,16 +43,15 @@ defmodule Membrane.Element.IBMSpeechToText.MixProject do
       {:ibm_speech_to_text, "~> 0.3.0"},
       {:membrane_file_plugin, "~> 0.16.0"},
       {:membrane_flac_plugin, "~> 0.11.0"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
